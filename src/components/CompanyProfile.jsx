1967ed1ff20ca1fb0dd6d57d0df218436dadff62
Juank628 <jsanchez_zunino@yahoo.com>
2023-06-03 23:49:27 -0500

    Correct linters

--- src/components/CompanyProfile.jsx
+++ src/components/CompanyProfile.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styles from './CompanyProfile.module.scss'
+import React from 'react';
+import styles from './CompanyProfile.module.scss';
 
 export default function CompanyProfile() {
   return (
     <div className={styles.container}>
-        <h2>Company profile</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsam voluptate officiis minima aut, similique fuga excepturi totam recusandae ut repellat corporis et, consectetur quibusdam beatae laborum rerum dolorum itaque?</p>
+      <h2>Company profile</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
     </div>
-  )
+  );
 }
--- src/components/TimeSpanSelector.jsx
+++ src/components/TimeSpanSelector.jsx
@@ -4,12 +4,12 @@ import styles from './TimeSpanSelector.module.scss';
 export default function TimeSpanSelector() {
   return (
     <div className={styles.container}>
-      <button className={styles.nonSelected}>10y</button>
-      <button className={styles.nonSelected}>5y</button>
-      <button className={styles.nonSelected}>1y</button>
-      <button className={styles.nonSelected}>6m</button>
-      <button className={styles.selected}>3m</button>
-      <button className={styles.nonSelected}>1m</button>
+      <button type="button" className={styles.nonSelected}>10y</button>
+      <button type="button" className={styles.nonSelected}>5y</button>
+      <button type="button" className={styles.nonSelected}>1y</button>
+      <button type="button" className={styles.nonSelected}>6m</button>
+      <button type="button" className={styles.selected}>3m</button>
+      <button type="button" className={styles.nonSelected}>1m</button>
     </div>
   );
 }
